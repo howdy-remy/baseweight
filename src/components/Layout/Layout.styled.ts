912271f43f58156c97fd4cdf0c553611e2048f49
Remy Lynch <howdyremy@gmail.com>
2025-02-16 19:22:05 -0500

    update sidebar

---
+++ src/components/Layout/Layout.styled.ts
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import { HeadingOne, TextSansRegular } from "components/Typography";
+import { Link } from "react-router";
+
+// layout
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 296px 1fr;
+  column-gap: 24px;
+  width: 100%;
+`;
+
+export const Sidebar = styled.div`
+  position: sticky;
+  top: 0;
+
+  height: 100vh;
+  width: 296px;
+  padding: ${({ theme }) => theme.spacing.l}px;
+  background-color: ${({ theme }) => theme.colors.flour};
+`;
+
+export const SidebarLinks = styled.div`
+  margin-top: ${({ theme }) => theme.spacing.l}px;
+`;
+
+// bits
+export const LogoType = styled(HeadingOne)`
+  color: ${({ theme }) => theme.colors.moss};
+  margin: 0;
+  line-height: 1.4;
+
+  & > span {
+    font-family: inherit;
+    color: ${({ theme }) => theme.colors.lichen};
+  }
+`;
+
+export const SidebarLink = styled(Link)`
+  display: block;
+  padding: ${({ theme: { spacing } }) => `${spacing.s}px ${spacing.m}px`};
+  border-radius: ${({ theme }) => theme.spacing.s}px;
+
+  color: ${({ theme }) => theme.colors.black};
+  text-decoration: none;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+export const AccountInfo = styled.div`
+  position: absolute;
+  bottom: ${({ theme }) => theme.spacing.l}px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: ${({ theme }) => theme.spacing.m}px;
+
+  width: ${({ theme }) => `calc(100% - ${theme.spacing.l * 2}px)`};
+  height: 40px;
+`;
+
+export const Username = styled(TextSansRegular)`
+  color: ${({ theme }) => theme.colors.black};
+  &:hover {
+    color: ${({ theme }) => theme.colors.moss};
+  }
+`;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,29 +1,59 @@
-import { type ReactNode, useState } from "react";
-import "./Layout.css";
-import { useAuth } from "../../contexts/Authentication";
-import { useGetProfileQuery } from "../../api/profile";
-import Avatar from "../Avatar/Avatar";
+import { Fragment, type ReactNode } from "react";
 import { Link } from "react-router";
-import { HeadingOne } from "../Typography/typography";
+
+import { useAuth } from "contexts/Authentication";
+import { useGetProfileQuery } from "api/profile";
+
+import Avatar from "../Avatar/Avatar";
+import {
+  LogoType,
+  Wrapper,
+  Sidebar,
+  AccountInfo,
+  Username,
+  SidebarLink,
+  SidebarLinks,
+} from "./Layout.styled";
+import { useGetPacksQuery } from "api/packs";
+import { TextSansBold } from "components/Typography";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   const { session } = useAuth();
   const { data: profile } = useGetProfileQuery({ userId: session?.user.id });
-
+  const { data: packs, isLoading } = useGetPacksQuery({
+    userId: session?.user.id,
+  });
   return (
-    <div className="wrapper">
-      <div className="sidebar">
-        <Link to="/">
-          <HeadingOne as="h1" style={{ margin: 0 }}>
-            Field Trip
-          </HeadingOne>
+    <Wrapper>
+      <Sidebar>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <LogoType as="h1">
+            base<span>weight</span>
+          </LogoType>
         </Link>
-        <p>
-          <Link to="/account">{profile?.username}</Link>
-        </p>
-        <Avatar url={profile?.avatar_url || null} size={40} />
-      </div>
+
+        <SidebarLinks>
+          <SidebarLink to="/">
+            <TextSansBold>All Gear</TextSansBold>
+          </SidebarLink>
+          <SidebarLink to="/">
+            <TextSansBold>Packs</TextSansBold>
+          </SidebarLink>
+          {packs?.map((pack) => (
+            <Fragment key={pack.id}>
+              <SidebarLink to={`/packs/${pack.id}`}>{pack.name}</SidebarLink>
+            </Fragment>
+          ))}
+        </SidebarLinks>
+
+        <Link to="/account" style={{ textDecoration: "none" }}>
+          <AccountInfo>
+            <Username>{profile?.username}</Username>
+            <Avatar url={profile?.avatar_url || null} size={40} />
+          </AccountInfo>
+        </Link>
+      </Sidebar>
       {children}
-    </div>
+    </Wrapper>
   );
 };
