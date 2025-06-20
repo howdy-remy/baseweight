09fcf0889e59d1cb268daa4377b8a6240b8fd96f
Remy Lynch <howdyremy@gmail.com>
2025-06-20 01:40:39 -0400

    responsive modals

--- src/components/Modal/Modal.styled.ts
+++ src/components/Modal/Modal.styled.ts
@@ -29,11 +29,14 @@ export const StyledDialogPanel = styled(DialogPanel)`
   border-radius: ${({ theme }) => theme.spacing.l}px;
   background-color: ${({ theme }) => theme.colors.white};
 
-  /* box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); */
   box-shadow:
     rgba(17, 17, 26, 0.1) 0px 4px 16px,
     rgba(17, 17, 26, 0.1) 0px 8px 24px,
     rgba(17, 17, 26, 0.1) 0px 16px 56px;
+
+  @media (max-width: 704px) {
+    width: ${({ theme }) => `calc(100% - ${theme.spacing.xl * 2}px)`};
+  }
 `;
 
 export const StyledDialogTitle = styled(DialogTitle)`
